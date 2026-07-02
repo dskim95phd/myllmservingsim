7470e9b12a4c5d188d1ab6db48e5658377d95156
dskim95phd <dskim95phd@users.noreply.github.com>
2026-07-02 12:53:39 +0900

    Install rich in simulator Docker setup

--- scripts/docker-sim.sh
+++ scripts/docker-sim.sh
@@ -14,6 +14,6 @@ docker run --name servingsim_docker \
   -v "$REPO_ROOT":/app/LLMServingSim \
   -w /app/LLMServingSim \
   astrasim/tutorial-micro2024 \
-  bash -c "pip3 install pyyaml pyinstrument transformers datasets \
+  bash -c "pip3 install pyyaml pyinstrument transformers datasets rich \
   msgspec scikit-learn xgboost==3.1.2 matplotlib==3.5.3 pandas==1.5.3 \
   numpy==1.23.5 && exec bash"
